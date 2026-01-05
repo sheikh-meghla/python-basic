69073bf9e0c6fde48cf3a639dd5109a625884a4e
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-05 14:50:19 +0600

    multiple-variables

---
+++ multiple_variables.py
@@ -0,0 +1,20 @@
+#Many Values to Multiple Variables
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(y)
+print(z)
+
+#One Value to Multiple Variables
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+
+#unpack variables
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
