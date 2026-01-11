40f8fe0f4eb447db1f52d157f71b2bc647c11e4a
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-11 19:06:26 +0600

    join set

--- join_set.py
+++ join_set.py
@@ -4,6 +4,8 @@ set4 = {"kiwi", "pineapple"}
 set2 = set.union(set1)
 set5 = set4 | set2
 set6 = set.intersection(set2)
+set7 = set.symmetric_difference(set1)
+set8 = set.difference(set1)
 print(set6)
 print(set5)
 print(set2)
