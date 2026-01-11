0ac4f78ccbd29ce1e565dc67c63509deac257d1c
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-11 18:29:38 +0600

    remove set

---
+++ remove_set.py
@@ -0,0 +1,12 @@
+#removing an item
+thisset = {"apple", "banana", "cherry"}
+thisset.discard("banana")
+print(thisset)
+
+#clearing the set
+thisset = {"apple", "banana", "cherry"}
+
+thisset.clear()
+
+
+
