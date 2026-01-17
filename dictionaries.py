b75861b258e557de9e45deb18fe43162b04cf770
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-17 20:50:05 +0600

    dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,5 +1,8 @@
 thisdic = {"name": "meghla",
            "phone": "01302809067", 
            "email": "meghla@gmail.com"}
-print(thisdic)
-print(thisdic["phone"])
+x = thisdic.values()
+print(x)
+thisdic["age"] = 17
+print(x)
+
