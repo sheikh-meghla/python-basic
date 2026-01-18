b2b6f70ce8ddd7923b63af45ff06170c89c53ee9
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-18 21:26:16 +0600

    add tuples example and type demonstration

---
+++ elif.py
@@ -0,0 +1,26 @@
+a = 100
+b = 40
+c = 30
+
+sobthekborovalue = 0
+
+if a > b and a > c:
+    sobthekborovalue = a
+
+elif b > a and b > c:
+   sobthekborovalue = b
+else:
+    sobthekborovalue = c
+
+print(sobthekborovalue)
+
+
+
+age = 20
+
+if age < 18:
+    print("child")
+
+elif age <= 20:
+    print("teenager")
+
