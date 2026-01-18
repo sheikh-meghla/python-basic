5bc0a2522e05443aaa76e75db55444692a0d21dd
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-18 12:35:08 +0600

    nested dictionaries

---
+++ nested_dictionaries.py
@@ -0,0 +1,18 @@
+myfamily = {
+  "child1" : {
+    "name" : "Nurani",
+    "age" : 28
+  },
+  "child2" : {
+    "name" : "Rafi",
+    "age" : 24
+  },
+  "child3" : {
+    "name" : "Liza",
+    "age" : 17
+  }
+}
+
+print(myfamily)
+
+#print(myfamily["child2"]["name"])
