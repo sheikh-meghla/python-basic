79c86d6c893abdf672eab767f036ef8176560b04
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-18 12:19:21 +0600

    loop dictionary

---
+++ loop_dictionary.py
@@ -0,0 +1,14 @@
+thisdict = {
+  "name": "Meghla",
+  "phone": "01302809067",
+  "age": 17
+}
+for x in thisdict:
+    print(x)
+
+
+
+# keys and velue 
+
+#for x, y in thisdict.items():
+ # print(x, y)
