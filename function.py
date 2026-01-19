0182900fdd3a8b6dd729b4fe6d12e58dcfbcbf98
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-19 15:44:32 +0600

    python function complete

---
+++ function.py
@@ -0,0 +1,17 @@
+def Meghla():
+  print("Hello World")
+Meghla()
+
+age = 50
+Meghla = (age - 20) * 5 / 9
+print(Meghla)
+
+age = 70
+Nurani = (age - 20) * 5 / 9
+print(Nurani)
+
+age = 60
+Rafi = (age - 20) * 5 / 9
+print(Rafi)
+
+
