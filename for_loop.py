ef22bfaf0867e1ef20d3091de6949fa2ff3c460e
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-19 15:25:10 +0600

    python for_loop complete

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+days = ["Saturday", "Sunday", "Monday","Tuesday","Wednesday"]
+for x in days:
+  print(x) 
+  if x == "Monday":
+    break
+#
+
+days = ["Saturday", "Sunday", "Monday","Tuesday","Wednesday"]
+for x in days:
+  if x == "Monday":
+    break
+  print(x) 
+
+
+for x in range(9):
+  print(x) 
