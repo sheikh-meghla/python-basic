38f4200c571ad4ebc3310f40e9ee3f73aa56e08f
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-19 15:16:37 +0600

    python while_loop complete

---
+++ while_loop.py
@@ -0,0 +1,16 @@
+i = 1
+while i < 100:
+  print(i)
+  if (i == 6):
+    break
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+
+#i = 1
+#while i < 6:
+  #print(i)
+  #i += 1
+#else:
+#  print("i is no longer less than 6")
