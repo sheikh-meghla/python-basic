31b090f11b5efb52dacba0a5d777d12f37d0a704
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-21 17:47:02 +0600

    python arrays

---
+++ arrays.py
@@ -0,0 +1,22 @@
+x = ["Megla", "Nurani", "Rafi"]
+print(x)
+
+
+#
+
+a = ["A", "B", "C"]
+a = a[0]
+print(a)
+
+#
+
+a = ["A", "B", "C"]
+a[0] = "MEGHLA"
+print(a)
+
+#
+
+cars = ["Ford", "Volvo", "BMW"]
+
+for x in cars:
+  print(x)
