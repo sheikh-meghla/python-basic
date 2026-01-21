174b291136a8c149b835bb9027a9fbc70b425896
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-21 17:55:32 +0600

    python iterators

---
+++ iterators.py
@@ -0,0 +1,17 @@
+x = ("Meghla", "Rafi", "Nurani")
+a = iter(x)
+
+print(next(a))
+print(next(a))
+print(next(a))
+
+#
+
+x = "MEGHLA"
+a = iter(x)
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
